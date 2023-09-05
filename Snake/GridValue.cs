cefc312bb7bd44098c30d922ecf1e7fada790e97
Almas Arslanov <crazynick21@gmail.com>
2023-09-05 11:08:09 +0300

    Добавление класса положений на игровом поле

---
+++ Snake/GridValue.cs
@@ -0,0 +1,13 @@
+﻿namespace Snake
+{
+    /// <summary>
+    /// Положения
+    /// </summary>
+    public enum GridValue
+    {
+        Empty, // Положение пустых клеток 
+        Snake, // Положение змеи
+        Food, // Положение еды
+        Outside // Выход за рамки
+    }
+}
