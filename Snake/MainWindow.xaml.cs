d09f40a08c38fe997a72755699dc9cb3e1a15e64
Rookin21 <crazynick21@gmail.com>
2023-09-06 23:39:45 +0300

    Реализация логики добавления картинок в поле

--- Snake/MainWindow.xaml.cs
+++ Snake/MainWindow.xaml.cs
@@ -20,9 +20,42 @@ namespace Snake
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly int rows = 15, cols = 15; // количество рядов и столбцов
+        private readonly Image[,] gridImages; // Массив для вывода картинок
+
         public MainWindow()
         {
             InitializeComponent();
+            gridImages = SetupGrid();
+        }
+
+        /// <summary>
+        /// Вызов необходимых картинок на игровое поле
+        /// </summary>
+        /// <returns></returns>
+        private Image[,] SetupGrid()
+        {
+            Image[,] images = new Image[rows, cols];
+            GameGrid.Rows = rows;
+            GameGrid.Columns = cols;
+
+            for (int r = 0; r < rows; r++)
+            {
+                for(int c = 0; c < cols; c++)
+                {
+                    // Для каждой позиции создаем новую картинку
+
+                    Image image = new Image
+                    {
+                        Source = Images.Empty // Изначально будет пустая картинка
+                    };
+
+                    images[r, c] = image; // Складываем эту картинку в массив
+                    GameGrid.Children.Add(image); // Добавляем в потомка
+                }
+            }
+
+            return images;
         }
     }
 }
