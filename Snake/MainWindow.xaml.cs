d5cad920bb5c3d2a6b777d0a8fcc974560544983
Almas Arslanov <crazynick21@gmail.com>
2023-09-07 08:17:02 +0300

    1) Добавлены обработчики событий: загрузки окна, нажатия клавиши 2) Распределение картинок по соответствующим ячейкам

--- Snake/MainWindow.xaml.cs
+++ Snake/MainWindow.xaml.cs
@@ -20,13 +20,33 @@ namespace Snake
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Распеределение картинок к соответствующим ячейкам
+        private readonly Dictionary<GridValue, ImageSource> gridValToImage = new()
+        {
+            { GridValue.Empty, Images.Empty }, // Если пустая ячейка, то картинка "Empty" 
+            { GridValue.Snake, Images.Body }, // Если ячейка со змеей, то картинка "Body" 
+            { GridValue.Food, Images.Food }, // Если ячейка со едой, то картинка "Food" 
+        };
+
         private readonly int rows = 15, cols = 15; // количество рядов и столбцов
         private readonly Image[,] gridImages; // Массив для вывода картинок
+        private GameState gameState; // Вызов класса GameState
 
         public MainWindow()
         {
             InitializeComponent();
             gridImages = SetupGrid();
+            gameState = new GameState(rows, cols);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Draw();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+
         }
 
         /// <summary>
@@ -56,6 +76,24 @@ namespace Snake
             }
 
             return images;
+        }
+
+        private void Draw()
+        {
+            DrawGrid();
+        }
+
+        private void DrawGrid()
+        {
+            // Прохождение по все ячейкам
+            for (int r = 0; r < rows; r++) 
+            {
+                for (int c = 0; c < cols; c++)
+                { 
+                    GridValue gridVal = gameState.Grid[r, c]; // Получаем значение ячейки на конкретной позиции
+                    gridImages[r, c].Source = gridValToImage[gridVal]; // Вызов соответствующей картинки из словаря
+                }
+            }
         }
     }
 }
